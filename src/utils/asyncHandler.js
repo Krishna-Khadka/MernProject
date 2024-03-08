c7c74d8b5aeac884822665740e0debe4119fab2d
Krishna-Khadka <krishkhadka2059@gmail.com>
2024-03-08 13:30:32 +0545

    utility files created

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,24 @@
+// promise way
+
+const asyncHandler = (requestHandler) => {
+    return (req, res, next) => {
+        Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err))
+    }
+}
+
+export {asyncHandler}
+
+
+// function that accept fucnctions as parameter or return is call Higher Order Function
+// try catch way
+
+// const asyncHandler = (fn) => async(req, res, next) => {
+//     try {
+//         await fn(req, res, next)
+//     } catch (error) {
+//         res.status(err.code || 500).json({
+//             success: false,
+//             mssage: err.message
+//         })
+//     }
+// }
